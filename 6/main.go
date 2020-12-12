849fe77cc25e88af42f1b48bdacf97f2840591e4
Jay Morrow <jay@moia.io>
2020-12-12 15:28:23 +0100

    sum for which all groups answered

--- 6/main.go
+++ 6/main.go
@@ -7,26 +7,53 @@ import (
 )
 
 func main() {
-	data, err := input.ReadFileSplitByEmptyLine(input.DefaultFilename)
+	data, err := input.ReadFileAsStrings(input.DefaultFilename)
 	if err != nil {
 		panic(err)
 	}
 
-	maps := make([]map[rune]interface{}, len(data))
-	sum := 0
-	for i, line := range data {
-		trimmed := strings.ReplaceAll(line, " ", "")
+	maps := make([]map[rune]int, len(data))
+	sumAny := 0
+	sumEveryone := 0
+
+	for i := 0; i < len(data); i++ {
+		str := ""
+		total := 0
+		for i < len(data) {
+			if data[i] == "" {
+				break
+			} else {
+				str += data[i]
+				total++
+
+				if i+1 != len(data) {
+					i++
+				} else {
+					break
+				}
+			}
+		}
+
+		trimmed := strings.ReplaceAll(str, " ", "")
 		trimmed = strings.ReplaceAll(trimmed, "\n", "")
 		fmt.Printf("%v", trimmed)
 
-		maps[i] = make(map[rune]interface{})
+		maps[i] = make(map[rune]int)
 		for _, char := range trimmed {
-			maps[i][char] = nil
+			maps[i][char]++
 		}
 
 		fmt.Printf(" has %v unique answers\n", len(maps[i]))
-		sum += len(maps[i])
+		sumAny += len(maps[i])
+
+		for _, v := range maps[i] {
+			if v == total {
+				sumEveryone++
+			}
+		}
 	}
-	
-	fmt.Printf("total unique answers %v\n", sum)
+
+	fmt.Printf("total unique answers %v\n", sumAny)
+	fmt.Printf("total which everyone in a group answered %v\n", sumEveryone)
+
 }
